e20ed070451fb2ee46d77f930269f7b2193b68c4
Lynix <81735347+lunarlynix@users.noreply.github.com>
2023-02-06 20:09:28 -0400

    Foreach

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,6 @@ pub struct Test {
 fn main() {
     hello();
 
-    // Input from User Console in Rust
-    let mut input = String::new();
-    print!("Please input something: ");
-    io::stdout().flush().expect("Failed to flush");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-
-    println!("{}", to_string(&Test{name: input, number: 200, number2: 3}).unwrap());
-
     // Foreach in Rust
     let mut v = vec![1u8, 2, 3, 4, 5];
     for i in &mut v {
